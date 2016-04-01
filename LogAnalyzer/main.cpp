a9af2bc9ddda088da2823d5c7b503a0fbf7e42d5
Luigi Maisto <luigi.maisto@gmail.com>
2016-04-01 16:02:48 +0200

    Initial Commit

---
+++ LogAnalyzer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LogAnalyzer
+//
+//  Created by Luigi Maisto on 01/04/16.
+//  Copyright © 2016 Luigi Maisto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
