07b7aa7aaa385ac5b84e336c32a5f0c531a7d4f2
Luigi Maisto <luigi.maisto@gmail.com>
2016-04-01 16:40:39 +0200

    First implementation commit

--- LogAnalyzer/main.cpp
+++ LogAnalyzer/main.cpp
@@ -7,8 +7,37 @@
 //
 
 #include <iostream>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+int opt;
 
 int main(int argc, const char * argv[]) {
+    
+    // Declare the supported options.
+    po::options_description desc("Allowed options");
+    
+    desc.add_options()
+    ("help", "produce help message")
+    ("compression", po::value<int>(), "set compression level")
+    ("optimization,O", po::value<int>(&opt)->default_value(10),"optimization level")
+    ;
+    
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+    
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
+    
+    if (vm.count("compression")) {
+        std::cout << "Compression level was set to "
+        << vm["compression"].as<int>() << ".\n";
+    } else {
+        std::cout << "Compression level was not set.\n";
+    }
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
